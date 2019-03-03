a01833865cfa4e3b54579d7f39fde7a989306fc3
Соловьев Олег Эдуардович <oleg.solovev@privatbank.ua>
2019-03-03 20:58:22 +0200

    RpcClient changed for multiple connections usage.

--- RabbitMQ.Client.Extensions/AsyncBasicRpcClient.cs
+++ RabbitMQ.Client.Extensions/AsyncBasicRpcClient.cs
@@ -12,78 +12,61 @@ using RabbitMQ.Client.Extensions.Models;
 using Microsoft.Extensions.Options;
 using System.Linq;
 using System.Threading.Tasks.Dataflow;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace RabbitMQ.Client.Extensions
 {
-    public interface IRpcClient: IDisposable
+    public interface IRpcClient
     {
         Task<byte[]> Call(RabbitTransportMessage message);
     }
 
     public class AsyncRpcClient : IRpcClient
     {
-        private readonly int _threadCount;
+        private int _threadCount;
         private readonly IRabbitConnectionManager _rabbitConnectionManager;
         private readonly RabbitQueue _requestQueue;
         private readonly RabbitQueue _replyQueue = RabbitQueue.GetDirectReplyToQueue();
-        private ConcurrentDictionary<IBasicProperties, IModel> _channelsInUse = new ConcurrentDictionary<IBasicProperties,IModel>();
-        private readonly BufferBlock<byte[]> _respQueue = new BufferBlock<byte[]>();
+        private readonly BufferBlock<byte[]> _response;
 
         public AsyncRpcClient(IOptions<RpcClientConfiguration> rpcClientConfiguration, IRabbitConnectionManager rabbitConnectionManager, ILogger<AsyncRpcClient> logger)
         {
             _requestQueue = rpcClientConfiguration.Value.RequestQueue;
-            _threadCount = rpcClientConfiguration.Value.ThreadCount;
+            _threadCount = rpcClientConfiguration.Value.ThreadCount;           
             _rabbitConnectionManager = rabbitConnectionManager;
-
-            for (int i = 0; i < _threadCount; i++)
-            {
-                var channel = _rabbitConnectionManager.GetChannel();
-                var props = channel.CreateBasicProperties();
-                var correlationId = Guid.NewGuid().ToString();
-                props.CorrelationId = correlationId;
-                props.ReplyTo = _replyQueue.QueueName;
-                _channelsInUse.TryAdd(props, channel);
-                var consumer = new AsyncEventingBasicConsumer(channel);
-                
-
-                consumer.Received += async (model, ea) =>
-                {
-                    var response = ea.Body;
-                    if (_channelsInUse.Keys.Select(x => x.CorrelationId).Contains(ea.BasicProperties.CorrelationId))
-                    {
-                        await _respQueue.SendAsync(response);
-                    }
-                };
-
-                channel.BasicConsume(_replyQueue.QueueName, true, consumer);
-            }
+            _response = new BufferBlock<byte[]>(new DataflowBlockOptions { MaxMessagesPerTask = 1 });
         }
 
         public async Task<byte[]> Call(RabbitTransportMessage message)
         {
-            
-                var enumerator = _channelsInUse.GetEnumerator();
-                var res = enumerator.MoveNext();
-
-                var props = enumerator.Current.Key;
-                var channel = enumerator.Current.Value;
-
-            channel.BasicPublish(
-                exchange: RabbitExchange.GetDefault().ExchangeName,
-                routingKey: _requestQueue.QueueName,
-                basicProperties: props,
-                body: message.BodyBytes);
-
-            return await _respQueue.ReceiveAsync();
-        }
-
-        public void Dispose()
-        {
-            foreach (var item in _channelsInUse)
+            using (var channel = _rabbitConnectionManager.GetChannel())
             {
-                item.Value.Close();
-                item.Value.Dispose();
+                //channel.BasicQos(0, (ushort)_threadCount, false);
+        
+                var consumer = new AsyncEventingBasicConsumer(channel);
+
+                var props = channel.CreateBasicProperties();
+                props.ReplyTo = _replyQueue.QueueName;
+                props.CorrelationId = Guid.NewGuid().ToString();
+                consumer.Received += async (model, ea) => {
+                    var response = ea.Body;
+                    if (ea.BasicProperties.CorrelationId == props.CorrelationId)
+                    {
+                        await _response.SendAsync(response);
+                    }
+                };
+
+                channel.BasicConsume(_replyQueue.QueueName, true, consumer);
+                channel.BasicPublish(
+                    exchange: RabbitExchange.GetDefault().ExchangeName,
+                    routingKey: _requestQueue.QueueName,
+                    basicProperties: props,
+                    body: message.BodyBytes);
+
+                return await _response.ReceiveAsync();
             }
+            
         }
     }
 }
