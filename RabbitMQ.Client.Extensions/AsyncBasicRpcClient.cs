c7175306ae200620e1b2a13345a7f0f44326dffd
Соловьев Олег Эдуардович <oleg.solovev@privatbank.ua>
2019-03-06 11:05:58 +0200

    RPC call timeout configuration added.

--- RabbitMQ.Client.Extensions/AsyncBasicRpcClient.cs
+++ RabbitMQ.Client.Extensions/AsyncBasicRpcClient.cs
@@ -2,18 +2,13 @@
 using System.Collections.Concurrent;
 using System.Text;
 using RabbitMQ.Client.Extensions.Interfaces;
-using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using RabbitMQ.Client.Extensions.Configuration;
-using RabbitMQ.Client.Extensions.Infrastructure;
 using RabbitMQ.Client.Extensions.Models;
 using Microsoft.Extensions.Options;
-using System.Linq;
-using System.Threading.Tasks.Dataflow;
-using System.Collections;
-using System.Collections.Generic;
+using System.Threading;
 
 namespace RabbitMQ.Client.Extensions
 {
@@ -24,25 +19,33 @@ namespace RabbitMQ.Client.Extensions
 
     public class AsyncRpcClient : IRpcClient
     {
+        private readonly ILogger _logger;
         private readonly IRabbitConnectionManager _rabbitConnectionManager;
         private readonly RabbitQueue _requestQueue;
         private readonly RabbitQueue _replyQueue = RabbitQueue.GetDirectReplyToQueue();
         private int? _threadCount;
+        private TimeSpan _timeout;
         private ConcurrentDictionary<string, TaskCompletionSource<byte[]>> _pendingMessages;
         private IModel _channel;
         private AsyncEventingBasicConsumer _consumer;
 
         public AsyncRpcClient(IOptions<RpcClientConfiguration> rpcClientConfiguration, IRabbitConnectionManager rabbitConnectionManager, ILogger<AsyncRpcClient> logger)
         {
+            _logger = logger;
             _requestQueue = rpcClientConfiguration.Value.RequestQueue;
-            _threadCount = rpcClientConfiguration.Value.ThreadCount;           
+            _threadCount = rpcClientConfiguration.Value.ThreadCount;
+            _timeout = rpcClientConfiguration.Value.Timeout ?? Timeout.InfiniteTimeSpan;
             _rabbitConnectionManager = rabbitConnectionManager;
             _pendingMessages = new ConcurrentDictionary<string, TaskCompletionSource<byte[]>>();
             _channel = rabbitConnectionManager.GetChannel();
             _consumer = new AsyncEventingBasicConsumer(_channel);
             _consumer.Received += async (model, ea) => {
                 _pendingMessages.TryRemove(ea.BasicProperties.CorrelationId, out var tcs);
-                if (tcs != null) tcs.SetResult(ea.Body);
+                if (tcs != null)
+                {
+                    tcs.SetResult(ea.Body);
+                }
+                else _logger.LogWarning("No result on {CorrelationId}", ea.BasicProperties.CorrelationId);
             };
             _channel.BasicQos(0,(ushort?)_threadCount ?? 1,false);
             _channel.BasicConsume(_replyQueue.QueueName,true,_consumer);
@@ -52,7 +55,10 @@ namespace RabbitMQ.Client.Extensions
         {
             var tcs = new TaskCompletionSource<byte[]>();
             var correlationId = Guid.NewGuid().ToString();
-
+            var cts = new CancellationTokenSource(_timeout);
+            cts.Token.Register(() => tcs.TrySetException(new TimeoutException(string.Format(
+                "Request timeout on {0}. {1} response not recieved in {2} ms!", correlationId, Encoding.UTF8.GetString(message.BodyBytes), _timeout.Milliseconds.ToString()
+                ))), false);
             _pendingMessages.TryAdd(correlationId, tcs);
 
             var props = _channel.CreateBasicProperties();
--- RabbitMQ.Client.Extensions/Configuration/RpcClientConfiguration.cs
+++ RabbitMQ.Client.Extensions/Configuration/RpcClientConfiguration.cs
@@ -8,5 +8,6 @@ namespace RabbitMQ.Client.Extensions.Configuration
     {
         public RabbitQueue RequestQueue { get; set; }
         public int? ThreadCount { get; set; }
+        public TimeSpan? Timeout { get; set; }
     }
 }
